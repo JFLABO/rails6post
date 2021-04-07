e42bb8b3ac51eb0c0113b49bdfad4fb777c28205
JFLABO[JP] <vortex003@msn.com>
2021-04-07 19:19:27 +0900

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,1 +1,30 @@
+var data = {
+      "activity":
+        {
+            gamenid: id,
+            lat:vlat,
+            lon:vlon,
+            alt:valt,
+            name:vname,
+            message:vmes
+        }
+    };
 
+    $.ajax({
+      type: "post",
+      url: "/activities",
+      data: JSON.stringify(data),
+      contentType: 'application/json',
+      dataType: "json",
+
+      success: function (data) {
+        console.log("success");
+        console.log(data);
+        $("#mes").val("");
+      },
+
+      error: function (data) {
+        console.log("error");
+        console.log(data);
+      }
+    });
